4cc2d93a485d08dc74414474287752b7ce665913
Julia Pickel <juliapickel@gmx.de>
2023-10-09 23:36:47 +0200

    task2 completed square.js

---
+++ components/Square/Square.js
@@ -0,0 +1,14 @@
+// Square
+function Square() {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  square.addEventListener("click", () => {
+    square.style.backgroundColor = getRandomColor(); // hier wichtig: Funktion aufrufen ...() und Klammern nicht vergessen !
+  });
+  return square;
+}
+const rootElement = document.getElementById("root");
+const squareElement = Square();
+rootElement.appendChild(squareElement);
+
+export { Square };
--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 import { getRandomColor } from "./utils/randomColor.js";
 import { Circle } from "./components/Circle/Circle.js";
+import { Square } from "./components/Square/Square.js";
 console.clear();
 
 const root = document.getElementById("root");
-
-// Square
-function Square() {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.addEventListener("click", () => {
-    square.style.backgroundColor = getRandomColor(); // hier wichtig: Funktion aufrufen ...() und Klammern nicht vergessen !
-  });
-  return square;
-}
-// const rootElement = document.getElementById("root");
-const squareElement = Square();
-rootElement.appendChild(squareElement);
 
 // Pentagon
 function Pentagon() {
